2b32a6ff89c8772df3b0c927c81e444a15fc7dd7
nefiserrano <nefi_3902@hotmail.com>
2026-07-08 20:14:00 -0600

    API key added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { fetchMovieFromOMDb } from './api.js';
 import { getSavedMovies, saveMovies } from './storage.js';
 import { updateURLParameters, loadFiltersFromURL } from './urlParams.js';
+const API_KEY = '51403b9a';
 
 let myMoviesDataset = [];
 
